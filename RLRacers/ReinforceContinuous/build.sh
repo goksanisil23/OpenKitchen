290d95e62ec68fcdaa20818cf65e67f2b9de8bb2
Goksan Isil <goksan.isil@zenseact.com>
2026-01-01 18:06:18 +0100

    update build script to cuda 13

--- RLRacers/ReinforceContinuous/build.sh
+++ RLRacers/ReinforceContinuous/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export PATH=/home/s0001734/Downloads/cuda-12.6/bin:$PATH
-export LD_LIBRARY_PATH=/home/s0001734/Downloads/cuda-12.6/lib64:$LD_LIBRARY_PATH
+export PATH=/home/s0001734/Downloads/cuda_13.0/bin:$PATH
+export LD_LIBRARY_PATH=/home/s0001734/Downloads/cuda_13.0/lib64:$LD_LIBRARY_PATH
+export __NV_PRIME_RENDER_OFFLOAD=1 
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 mkdir -p build
-cmake -DCMAKE_PREFIX_PATH=/home/s0001734/Downloads/libtorch/libtorch -DCMAKE_BUILD_TYPE=Release -S . -B build
+cmake -DCMAKE_PREFIX_PATH=/home/s0001734/Downloads/libtorch-shared-with-deps-2.9.1+cu130/libtorch -DCMAKE_BUILD_TYPE=Release -S . -B build
 cmake --build build
